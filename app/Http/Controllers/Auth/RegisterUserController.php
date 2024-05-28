4d4bc9212f4e63939079f848c9d9b06b633d1af0
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2024-05-27 23:13:36 -0300

    docs: Add Swagger documentation for RegisterUserController

--- app/Http/Controllers/Auth/RegisterUserController.php
+++ app/Http/Controllers/Auth/RegisterUserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Actions\Auth\RegisterUser;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterUserRequest;
+use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class RegisterUserController extends Controller
@@ -16,7 +17,38 @@ class RegisterUserController extends Controller
         $this->registerUser = $registerUser;
     }
 
-    public function __invoke(RegisterUserRequest $request)
+    /**
+     * @OA\Post(
+     *     path="/api/register",
+     *     summary="Register a new user",
+     *     description="Endpoint to register a new user ",
+     *     tags={"User"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"name","email","password"},
+     *             @OA\Property(property="name", type="string", example="Name Teste"),
+     *             @OA\Property(property="email", type="string", format="email", example="yoxig80660@qiradio.com"),
+     *             @OA\Property(property="password", type="string", format="password", example="1234")
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="User registered successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="User registered successfully.")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Failed to register user",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Failed to register user.")
+     *         )
+     *     )
+     * )
+     */
+    public function __invoke(RegisterUserRequest $request): JsonResponse
     {
         $data = $request->all();
         try {
